c827f12674a99cf2e7f832d96f735486f7e14b18
brkss <berkassebrahim@gmail.com>
2021-09-13 18:12:45 +0100

    🔨 handle long texts

--- web/src/components/base/MusicSearchElement.tsx
+++ web/src/components/base/MusicSearchElement.tsx
@@ -7,15 +7,26 @@ export const MusicSearchElement: React.FC<IResult> = ({
   name,
   image,
 }) => {
+  const handleLongText = (text: string) => {
+    if (text.length > 25) return text.slice(0, 22) + "...";
+    return text;
+  };
+
   return (
-    <Box h={"120px"} mt={"10px"}>
+    <Box
+      h={"120px"}
+      cursor={"pointer"}
+      transition={".3s"}
+      _hover={{ opacity: 0.7, transition: ".3s" }}
+      mt={"10px"}
+    >
       <Grid templateColumns="repeat(4, 1fr)">
-        <GridItem h={"100px"} colSpan={1} _hover={{ opacity: 0.7 }}>
+        <GridItem h={"100px"} colSpan={1}>
           <Image src={image} w={"120px"} rounded={"7px"} />
         </GridItem>
         <GridItem h={"100px"} p={"0 10px"} colSpan={3}>
-          <Heading>{name}</Heading>
-          <Text>{artist}</Text>
+          <Heading>{handleLongText(name)}</Heading>
+          <Text>{handleLongText(artist)}</Text>
         </GridItem>
       </Grid>
     </Box>
